efd2370830fc7f1634a0a67733b71bcbfbab48dd
Ievgen Khoptiar <ievgen.khoptiar@gmail.com>
2020-04-14 22:20:59 +0300

    disconnect issues fixed

--- app.js
+++ app.js
@@ -7,30 +7,39 @@ const userList = []
 const roomList = []
 
 io.on("connection", (socket) => {
-    socket.on("disconnect", function () {
-        console.log("user disconnected")
-        this.userList = null
+    socket.on("remove-user", (user) => {
+        userList.splice(userList.indexOf(user), 1)
+        io.emit("users", { type: "users", list: userList })
     })
 
     socket.on("message", (message) => {
-        console.log("Message Received: " + JSON.stringify(message))
+        // console.log("Message Received: " + JSON.stringify(message))
         io.emit("message", { type: "new-message", text: message })
     })
 
     socket.on("users", (users) => {
-        console.log("User: " + JSON.stringify(users))
+        socket.user_name = users
+        console.log("User: ", users)
         if (!userList.includes(users)) {
             userList.push(users)
         }
         io.emit("users", { type: "users", list: userList })
     })
 
+    socket.on("room", (room) => {
+        socket.join(room)
+    })
+
     socket.on("rooms", (rooms) => {
-        console.log("Room: " + JSON.stringify(rooms))
         if (!roomList.includes(rooms)) {
             roomList.push(rooms)
         }
         io.emit("rooms", { type: "rooms", list: roomList })
+    })
+
+    socket.on("disconnect", () => {
+        userList.splice(userList.indexOf(socket.user_name), 1)
+        io.emit("users", { type: "users", list: userList })
     })
 })
 
